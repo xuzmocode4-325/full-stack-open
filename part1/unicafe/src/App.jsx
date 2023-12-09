2a8774b8617093f6571c3c4fd90dff77972c5f7a
Katlego Thobye <katlegothobye@yahoo.com>
2023-12-09 23:40:29 +0200

    completed unicafe App.jsx tasks

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -17,7 +17,59 @@ const Buttons = ({good, neutral, bad}) => {
     </div>
   )    
 }
-const Statistics = ({data}) => {
+
+const StatsTable = ({data, log}) => {
+  const length = log.length
+
+  const reduction = log.reduce(
+    (x, y) => x + y, 
+    0,
+  );
+  const logGood = log.filter((element) => element === 1);
+  const numGood = logGood.reduce(
+    (x, y) => x + y, 
+    0,
+  ); 
+  
+  const average = reduction / length 
+  const percent = numGood / length * 100 
+
+  console.log('length', length)
+  console.log('average', average)
+  console.log('percent', percent)
+
+  return (  
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td>Good</td>
+            <td>{data.good}</td>
+          </tr>
+          <tr>
+            <td>Neutral</td>
+            <td>{data.neutral}</td>
+          </tr>
+          <tr>
+            <td>Bad</td>
+            <td>{data.bad}</td>
+          </tr>
+          <tr>
+            <td>Average</td>
+            <td>{average}</td>
+          </tr>
+          <tr>
+            <td>Percent Good  </td>
+            <td>{percent}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+
+}
+
+const Statistics = ({data, log}) => {
   console.log(data.good, data.neutral, data.bad)
   if (data.good === 0 & data.neutral === 0 & data.bad === 0) {
     return (
@@ -28,30 +80,34 @@ const Statistics = ({data}) => {
      
     )
   }
+  
   return (
     <div>
       <h3>Statistics</h3>
-      <p>Good: {data.good} </p>
-      <p>Neutral: {data.neutral}</p>
-      <p>Bad: {data.bad}</p>
+        <StatsTable data={data} log={log}/>
     </div>
   )
 }
 
 const App = () => {
+
+  const [log, setLog] = useState([])
   const [feedback, setFeedback] = useState({
     good: 0, neutral:0, bad:0
   })
 
   const handleGood = () => {
+    setLog(log.concat(1))
     console.log('good')
     setFeedback({...feedback, good: feedback.good + 1})
   }
   const handleNeutral = () => {
+    setLog(log.concat(0))
     console.log('neutral')
     setFeedback({...feedback, neutral: feedback.neutral + 1})
   }
   const handleBad = () => {
+    setLog(log.concat(-1))
     console.log('bad')
     setFeedback({...feedback, bad: feedback.bad + 1})
   }
@@ -60,7 +116,7 @@ const App = () => {
     <div>
      <Heading title='Give Us Feedback'/>
      <Buttons good={handleGood} neutral={handleNeutral} bad={handleBad}/>
-     <Statistics data={feedback}/>
+     <Statistics data={feedback}  log={log}/>
     </div>
   )
     
