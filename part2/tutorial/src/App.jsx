4b52b77cc7e16d7f116a2289033706e742e4efd0
Katlego Thobye <katlegothobye@yahoo.com>
2023-12-12 19:34:46 +0200

    completed fetching data from server tutorial in notes app

--- part2/tutorial/src/App.jsx
+++ part2/tutorial/src/App.jsx
@@ -1,10 +1,24 @@
-import {useState} from 'react'
+import axios from 'axios'
+import {useState, useEffect} from 'react'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [showAll, setShowAll] = useState(true)
   const [newNote, setNewNote] = useState('')
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+  useEffect(hook, [])
+  console.log('render', notes.length, 'notes')
+
 
   const addNote = (event) => {
     event.preventDefault()
