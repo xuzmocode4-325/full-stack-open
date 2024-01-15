6c6c73122ea07e03f3cf04a7c99d489020c01c0c
Katlego Thobye <katlegothobye@yahoo.com>
2024-01-15 17:42:39 +0200

    added nodemon

--- part3/tutorial/index.js
+++ part3/tutorial/index.js
@@ -1,4 +1,5 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
 let notes = [
     {
@@ -17,11 +18,16 @@ let notes = [
       important: true
     }
   ]
-  const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/json' })
-    response.end(JSON.stringify(notes))
-  })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+  app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
+  
+  app.get('/api/notes', (request, response) => {
+    response.json(notes)
+  })
+  
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
