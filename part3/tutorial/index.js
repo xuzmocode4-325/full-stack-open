2bf1064207fabc12ec650d5dd1f57c7fd5ff2edd
Katlego Thobye <katlegothobye@yahoo.com>
2024-01-30 17:26:08 +0200

    update index.js

--- part3/tutorial/index.js
+++ part3/tutorial/index.js
@@ -26,8 +26,30 @@ let notes = [
   app.get('/api/notes', (request, response) => {
     response.json(notes)
   })
+
+  app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    
+    if (note) {
+      response.json(note)
+    } else {
+      response.statusMessage = "Note not found";
+      response.status(404).end()
+    }
+    //console.log(note)
+  })
+
+  app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+  
+    response.status(204).end()
+  })
   
   const PORT = 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
+
+  
